bd4982133dafdce3bf1168b36189c77b96498d6c
Ibrohim <ibrohim.rashidov9199@gmail.com>
2024-06-22 15:45:35 +0500

    First

---
+++ main.py
@@ -0,0 +1,44 @@
+"""
+Lesson 2  5-Oy  Python Advanced
+Decimal, float
+
+"""
+import math
+from decimal import Decimal
+import requests
+# import math
+# x = 15
+# # + - * / ** % //
+# r = math.sqrt(x)
+# print(round(3.1))
+# print(math.ceil(3.001))
+# print(math.floor(3.99))
+# print(divmod(12, 5))
+# print(pow(5, 5))
+
+
+# price = 10.25
+# minutes, second = 5, 34
+# total_minute = (minutes*60+second)/60
+# summa = math.ceil(total_minute) * price
+# print(summa)
+# e = 100_000_000
+# x = Decimal('0.1')
+# y = Decimal('0.1')
+# z = Decimal('0.1')
+# print(x+y+z == Decimal('0.3'))
+
+# q = Decimal('0.001')
+# x = Decimal('3.14568574745')
+# s = x.quantize(q)
+# print(s)
+
+# from PIL import Image
+#
+# i = Image.open('galaxsy.jpg')
+# new_image = i.resize((113, 151))
+# new_image.save('resize.jpg')
+
+
+
+
